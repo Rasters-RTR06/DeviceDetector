055a546c9415805ecfcbf2a397519c49d8571afb
imranpathan23 <imrans.android23@gmail.com>
2025-03-21 18:24:15 +0530

    FirstDraft

---
+++ DeviceDetector.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "resource.h"
+
+BOOL bRegistrationPage = FALSE;
+BOOL bLoginPage = FALSE;
+BOOL bForgotPwdPage = FALSE;
+BOOL bDeviceListPage = FALSE;
+BOOL bBlackListPage = FALSE;
+
+
+void AddMenu(HWND);
+
+void CreateDialogControls(HWND);
+
+void ShowHidePage(HWND);
