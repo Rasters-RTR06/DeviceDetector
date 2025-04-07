070c351e5fedfd0284761395b4ae50d9c5866588
imranpathan23 <imrans.android23@gmail.com>
2025-04-07 16:47:42 +0530

    Fix : CPP and C file integration
    # Conflicts:
    #	Build.bat
    #	DeviceDetector.cpp

--- DeviceDetector.cpp
+++ DeviceDetector.cpp
@@ -20,7 +20,7 @@
 // DEFINE_GUID(GUID_DEVINTERFACE_DISK, 0x53f56307L, 0xb6bf, 0x11d0, 0x94, 0xf2, 0x00, 0xa0, 0xc9, 0x1e, 0xfb, 0x8b);
 
 
-#include "Headers/DeviceDetector.h"
+#include "DeviceDetector.h"
 #include "Headers/DeviceDetector_JSON.h"
 
 
